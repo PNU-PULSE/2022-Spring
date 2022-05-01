13899337d20a43a078006c3c4fa58d42d10ab73c
red0825 <65748633+red0825@users.noreply.github.com>
2022-05-01 20:11:13 +0900

    Update BOJ_1112.cpp

--- Simulation/BOJ_1112/BOJ_1112.cpp
+++ Simulation/BOJ_1112/BOJ_1112.cpp
@@ -27,9 +27,9 @@ void input(){
 }
 
 void solution(){
-	int nx = x, nb = abs(b), temp;
-	if(b > 0) nx = abs(nx);
-	while(nx != 0){
+    int nx = x, nb = abs(b), temp;
+    if(b > 0) nx = abs(nx);
+    while(nx != 0){
         temp = nx % nb;
         if(temp < 0) temp += nb;
         result += (char)('0' + temp);
